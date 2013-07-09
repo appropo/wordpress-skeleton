b112e4e411a4255a878ad707ab432de03226842a
Moritz Haller <haller@appropo.net>
2013-07-09 20:59:07 +0200

    Uncomment dummy require mu-plugin, which breaks the skeleton

--- content/mu-plugins/load-plugins.php
+++ content/mu-plugins/load-plugins.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 /* ------------------------------------------------------------------------
- * PHP loader, to load all the plugins in subdirectories. 
+ * PHP loader, to load all the plugins in subdirectories.
  *
  * WordPress only looks for PHP files right inside the mu-plugins direcotry
  * and (unlike for normal plugins) not for files in subdirectories
  * ------------------------------------------------------------------------ */
 
-require WPMU_PLUGIN_DIR . '/path-to-subdir/plugin-name.php';
+// require WPMU_PLUGIN_DIR . '/path-to-subdir/plugin-name.php';
