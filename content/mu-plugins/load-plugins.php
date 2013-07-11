3d85739630c9b04424c66025860ffcc5f6e29dbb
Timo Kujawa <kujawa@appropo.net>
2013-07-11 23:00:18 +0200

    Update mu-plugins loader files

--- content/mu-plugins/load-plugins.php
+++ content/mu-plugins/load-plugins.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 /* ------------------------------------------------------------------------
- * PHP loader, to load all the plugins in subdirectories. 
+ * Plugin Name: Plugin Loader
+ * Description: PHP loader to load all the plugins in subdirectories.
  *
  * WordPress only looks for PHP files right inside the mu-plugins direcotry
  * and (unlike for normal plugins) not for files in subdirectories
  * ------------------------------------------------------------------------ */
 
-require WPMU_PLUGIN_DIR . '/path-to-subdir/plugin-name.php';
+//require WPMU_PLUGIN_DIR . '/path-to-subdir/plugin-name.php';
